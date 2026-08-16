7b90f2e7306a34668e6f50c3890cf883ae90d5d5
Gernot <119130972+GernotAlthammer@users.noreply.github.com>
2026-08-16 19:33:48 +0200

    Refactor config flow for KEBA P30 Modbus TCP

--- custom_components/keba_modbus_tcp/config_flow.py
+++ custom_components/keba_modbus_tcp/config_flow.py
@@ -1,21 +1,29 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 from .const import DOMAIN, CONF_HOST
 
+
 class KebaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for KEBA P30."""
+    """Handle a config flow for KEBA P30 Modbus TCP."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial setup step."""
         errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(title=f"KEBA P30 ({user_input[CONF_HOST]})", data=user_input)
+            return self.async_create_entry(
+                title=f"KEBA P30 ({user_input[CONF_HOST]})",
+                data=user_input,
+            )
 
         data_schema = vol.Schema({
             vol.Required(CONF_HOST): str,
         })
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=errors
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
         )
