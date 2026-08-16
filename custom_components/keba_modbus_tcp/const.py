09f9064ccdd9215685c349eb9b2d117bab0803ea
Gernot <119130972+GernotAlthammer@users.noreply.github.com>
2026-08-16 19:17:18 +0200

    Add constants for KEBA Modbus TCP configuration

---
+++ custom_components/keba_modbus_tcp/const.py
@@ -0,0 +1,5 @@
+DOMAIN = "keba_modbus_tcp"
+CONF_HOST = "host"
+# KEBA Modbus TCP Spezifikationen
+KEBA_PORT = 502          # Standard Modbus TCP Port
+KEBA_UNIT_ID = 255       # Zwingende Slave-Adresse laut Spezifikation
